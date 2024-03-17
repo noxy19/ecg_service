88223099e11af15449982ef2568bb2ed24ff1724
Joan Barba <joanbarba@proton.me>
2024-03-17 18:09:33 +0100

    model for the electrocardiogram domain

---
+++ src/domain/model.py
@@ -0,0 +1,20 @@
+from typing import Optional, Literal
+from uuid import UUID
+from dataclasses import dataclass
+from pydantic import BaseModel
+
+
+LeadType = Literal["I", "II", "III", "aVR", "aVL", "aVF", "V1", "V2", "V3", "V4", "V5", "V6"]
+
+
+class ElectrocardiogramLead(BaseModel):
+    name: LeadType
+    num_samples: Optional[int]
+    signal: list[int]
+
+
+class Electrocardiogram(BaseModel):
+    id: UUID
+    user_id: UUID
+    date: str
+    leads: list[ElectrocardiogramLead]
