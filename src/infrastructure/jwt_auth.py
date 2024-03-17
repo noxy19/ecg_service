6bd34573375b7b1ca563a276659dc6834f8f78d4
Joan Barba <joanbarba@proton.me>
2024-03-17 18:15:33 +0100

    Implement jwt auth verification system

---
+++ src/infrastructure/jwt_auth.py
@@ -0,0 +1,37 @@
+from fastapi import Depends, HTTPException, Security
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from typing import Optional
+from pydantic import BaseModel
+import jwt
+from src.application.settings import Settings
+from .providers import settings_provider
+import logging
+from uuid import UUID
+
+logger = logging.getLogger()
+security = HTTPBearer()
+
+
+class JwtClaims(BaseModel):
+    name: str
+    sub: UUID
+
+
+def decode_jwt(token: str, secret_key: str, algorithm: str) -> Optional[dict]:
+    try:
+        decoded_token = jwt.decode(token, secret_key, algorithms=[algorithm])
+        return decoded_token if decoded_token else None
+    except jwt.PyJWTError:
+        return None
+
+
+async def get_current_user(
+    credentials: HTTPAuthorizationCredentials = Security(security), settings: Settings = Depends(settings_provider)
+) -> JwtClaims:
+    try:
+        token = credentials.credentials
+        decoded_token = decode_jwt(token, settings.jwt_secret_key, settings.jwt_algorithm)
+        return JwtClaims.parse_obj(decoded_token)
+    except Exception as e:
+        logger.error(e)
+        raise HTTPException(status_code=401, detail="Invalid authentication credentials")
