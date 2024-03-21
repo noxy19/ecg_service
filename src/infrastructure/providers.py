e05bf04d10ecef07fdd5f472c391117e99a8a2c6
Joan Barba <joanbarba@proton.me>
2024-03-21 17:14:59 +0100

    Implement providers

--- src/infrastructure/providers.py
+++ src/infrastructure/providers.py
@@ -1,8 +1,13 @@
 from src.application.settings import Settings
 from src.domain.message_service import MessageService
+from src.domain.repository import EcgRepository
 from src.infrastructure.sqs_message_service import SqsMessageService
 from src.application.create_ecg_use_case import CreateEcgUseCase
+from src.application.get_insights_use_case import GetInsightsUseCase
+from src.application.user_use_case import UserUseCase
+from src.infrastructure.postgres_repository import PostgresEcgRepository
 from fastapi import Depends
+from psycopg_pool import AsyncConnectionPool
 
 
 def settings_provider() -> Settings:
@@ -19,7 +24,22 @@ def message_service_provider(settings: Settings = Depends(settings_provider)) ->
     )
 
 
+async def repository_provider(settings: Settings = Depends(settings_provider)) -> EcgRepository:
+    connectionPool = AsyncConnectionPool(f"postgresql://{settings.postgres_user}:{settings.postgres_password}@db")
+    return PostgresEcgRepository(connectionPool)
+
+
 def create_ecg_use_case_provider(
     message_service: MessageService = Depends(message_service_provider),
 ) -> CreateEcgUseCase:
     return CreateEcgUseCase(message_service=message_service)
+
+
+def get_insights_use_case_provider(repository: EcgRepository = Depends(repository_provider)) -> GetInsightsUseCase:
+    return GetInsightsUseCase(repository)
+
+
+def user_use_case_provider(
+    repository: EcgRepository = Depends(repository_provider), settings: Settings = Depends(settings_provider)
+) -> UserUseCase:
+    return UserUseCase(repository, settings)
