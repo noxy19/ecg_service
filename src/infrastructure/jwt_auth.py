81c1919993d848c7b97e460124309872defc1923
Joan Barba <joanbarba@proton.me>
2024-03-21 17:09:21 +0100

    Add auth system to validate admin user

--- src/infrastructure/jwt_auth.py
+++ src/infrastructure/jwt_auth.py
@@ -7,6 +7,7 @@ from src.application.settings import Settings
 from .providers import settings_provider
 import logging
 from uuid import UUID
+import bcrypt
 
 logger = logging.getLogger()
 security = HTTPBearer()
@@ -35,3 +36,11 @@ async def get_current_user(
     except Exception as e:
         logger.error(e)
         raise HTTPException(status_code=401, detail="Invalid authentication credentials")
+
+
+async def is_admin_user_valid(
+    credentials: HTTPAuthorizationCredentials = Security(security), settings: Settings = Depends(settings_provider)
+) -> bool:
+    hashed = bcrypt.hashpw(settings.admin_password.encode("utf-8"), bcrypt.gensalt())
+    provided_password = credentials.credentials
+    return bcrypt.checkpw(provided_password.encode("utf-8"), hashed)
