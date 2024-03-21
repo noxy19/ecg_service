dd0c18331806d9e8e0a597ca9c9b795d48644ff2
Joan Barba <joanbarba@proton.me>
2024-03-21 17:11:05 +0100

    Implement processor of the events

--- src/application/process_ecg_use_case.py
+++ src/application/process_ecg_use_case.py
@@ -2,20 +2,27 @@ import logging
 from result import Result, Ok
 from src.domain.count_zero_crossings_service import CountZeroCrossingsService
 from src.domain.messages import PostEcgEvent
+from src.domain.model import EcgLeadInsight
+from src.domain.repository import EcgRepository
+from psycopg_pool import AsyncConnectionPool
 
 logger = logging.getLogger()
 
 
 class ProcessEcgUseCase:
 
-    def __init__(self, zero_crossings: CountZeroCrossingsService):
+    def __init__(self, zero_crossings: CountZeroCrossingsService, repository: EcgRepository):
         self._zero_crossings = zero_crossings
+        self._repository = repository
 
     async def execute(self, event: PostEcgEvent) -> Result[None, Exception]:
-        logger.info("ProcessEcgUseCase event recieved")
-        for lead in event.electrocardiogram.leads:
-            zero_crossings = self._zero_crossings.count(lead.signal)
-            logger.info(f'ZERO CROSSINGS: {zero_crossings}, for lead {lead.name}')
-
-        # db call
+        lead_insights = [
+            EcgLeadInsight(
+                ecg_id=event.electrocardiogram.id,
+                lead_name=lead.name,
+                zero_crossings=self._zero_crossings.count(lead.signal),
+            )
+            for lead in event.electrocardiogram.leads
+        ]
+        result = await self._repository.save(event.electrocardiogram, lead_insights)
         return Ok(None)
